0920d68d880a0685f0df6cbc7402a299012d811f
Stella-Ray-Chaves <stellachavesyoga@gmail.com>
2018-07-06 11:18:05 -0500

    ggogle fonts moved to js

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -12,3 +12,21 @@ function logCRP() {
 window.addEventListener("load", function(event) {
   logCRP();
 });
+
+<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+<script type="text/javascript">
+	google.load('webfont','1');
+	google.setOnLoadCallback(function() {
+		WebFont.load({
+			google: {
+				families: ['Open Sans']
+			}
+		});
+	});
+</script>
+
+WebFont.load({
+	google: {
+		families: ['Open Sans']
+	}
+});
