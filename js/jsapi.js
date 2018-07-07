d1ce471ad01b2a0806b24a2cadf35915147f34b6
Stella-Ray-Chaves <stellachavesyoga@gmail.com>
2018-07-07 13:48:46 -0500

    moved google fonts to new file jsapi.js

---
+++ js/jsapi.js
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+<script type="text/javascript">
+	google.load('webfont','1');
+	google.setOnLoadCallback(function() {
+		WebFont.load({
+			google: {
+				families: ['Open Sans']
+			}
+		});
+	});
+</script>
