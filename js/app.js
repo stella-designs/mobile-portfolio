88f901bdc1d949f7178853eb42474f2ee56363af
Stella-Ray-Chaves <stellachavesyoga@gmail.com>
2018-07-24 13:35:02 -0500

    install service worker

--- js/app.js
+++ js/app.js
@@ -8,4 +8,23 @@ if ('serviceWorker' in navigator) {
       console.log('ServiceWorker registration failed: ', err);
     });
   });
-}
+};
+
+self.addEventListener('install', function(event) {
+    var CACHE_NAME = 'mobile-profile-cache-v1';
+    var urlsToCache = [
+    '/styles/style.css',
+    '/script/perfmatters.js'
+    ];
+
+    self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+    });
+  });
