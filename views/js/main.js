e2591dda29aeda8139a64b6722d5cf421cc4459e
Stella-Ray-Chaves <stellachavesyoga@gmail.com>
2018-08-22 14:26:20 -0500

    increased background pizza size

--- views/js/main.js
+++ views/js/main.js
@@ -551,8 +551,8 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.alt = "pizza";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
+    elem.style.height = "200px";
+    elem.style.width = "146.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
