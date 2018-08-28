3c75f352e4691517329b841823767001b460d8a0
Stella-Ray-Chaves <stellachavesyoga@gmail.com>
2018-08-28 12:00:38 -0500

    compressed js file

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,14 +1,1 @@
-// Measuring the Critical Rendering Path with Navigation Timing
-// https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
-
-function logCRP() {
-  var t = window.performance.timing,
-    dcl = t.domContentLoadedEventStart - t.domLoading,
-    complete = t.domComplete - t.domLoading;
-  var stats = document.getElementById("crp-stats");
-  stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
-}
-
-window.addEventListener("load", function(event) {
-  logCRP();
-});
+function logCRP(){var c=window.performance.timing,d=c.domContentLoadedEventStart-c.domLoading,a=c.domComplete-c.domLoading;var b=document.getElementById("crp-stats");b.textContent="DCL: "+d+"ms, onload: "+a+"ms"}window.addEventListener("load",function(a){logCRP()});
